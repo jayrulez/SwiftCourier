6ee2019ade1a2badaefdfda1cb71b420951385ea
Robert Campbell <waprave@gmail.com>
2016-01-11 00:00:47 -0500

    Another checkpoint

--- src/SwiftCourier/Controllers/BookingsController.cs
+++ src/SwiftCourier/Controllers/BookingsController.cs
@@ -25,6 +25,7 @@ namespace SwiftCourier.Controllers
                 .Include(b => b.Customer)
                 .Include(b => b.Package)
                 .Include(b => b.Invoice)
+                .Include(b => b.CreatedBy)
                 .Include(b => b.Service).ToListAsync();
 
             return View(bookings.ToListViewModel());
@@ -47,42 +48,7 @@ namespace SwiftCourier.Controllers
             return View(booking.ToDetailsViewModel());
         }
 
-        public async Task<IActionResult> Package(int id)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Package.PackageLogs)
-                .Include(b => b.Service)
-                .Include(b => b.CreatedBy)
-                .SingleAsync(m => m.Id == id);
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(booking.ToDetailsViewModel());
-        }
-
         public async Task<IActionResult> Invoice(int id)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Service)
-                .Include(b => b.CreatedBy)
-                .SingleAsync(m => m.Id == id);
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            return View(booking.ToDetailsViewModel());
-        }
-
-        public async Task<IActionResult> BillOfLading(int id)
         {
             var booking = await _context.Bookings
                 .Include(b => b.Customer)
@@ -220,152 +186,6 @@ namespace SwiftCourier.Controllers
 
             ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Name", model.CustomerId);
             ViewData["ServiceId"] = new SelectList(_context.Services, "Id", "Name", model.ServiceId);
-
-            return View(model);
-        }
-
-        public async Task<IActionResult> Dispatch(int id)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Service)
-                .SingleAsync(m => m.Id == id);
-
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Dispatch(int id, DispatchViewModel model)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Package.PackageLogs)
-                .Include(b => b.Service)
-                .SingleAsync(m => m.Id == id);
-
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            int userId;
-
-            if (!int.TryParse(HttpContext.User.GetUserId(), out userId))
-            {
-                //XXX:TODO Gracefully handle this
-                throw new Exception("Unable to get logged in User Id.");
-            }
-
-            if (ModelState.IsValid)
-            {
-                booking.Package.AssignedToUserId = model.UserId;
-
-                //XXX:TODO Fix when couriers are able to receive packages to their personal inventory
-                // Update status to OutForDelivery when the courier has confirmed receipt of the package
-                // For now, set as out for delivery when package is dispatched to courier
-                //booking.Package.Status = PackageStatus.DispatchedToCourier;
-                booking.Package.Status = PackageStatus.OutForDelivery;
-
-                var packageLog = new PackageLog() {
-                    PackageId = booking.Package.BookingId,
-                    //XXX:TODO See comment above
-                    //LogMessage = "Dispatched To Courier",
-                    LogMessage = string.Format("Received By Courier {0}.", model.UserId),
-                    LoggedAt = DateTime.Now
-                };
-
-                booking.Package.PackageLogs.Add(packageLog);
-
-                _context.Update(booking);
-
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction("Package", "Booking", new { id = booking.Id });
-            }
-
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
-
-            return View(model);
-        }
-
-        public async Task<IActionResult> Deliver(int id)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Service)
-                .SingleAsync(m => m.Id == id);
-
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Deliver(int id, DeliverViewModel model)
-        {
-            var booking = await _context.Bookings
-                .Include(b => b.Customer)
-                .Include(b => b.Invoice)
-                .Include(b => b.Package)
-                .Include(b => b.Package.PackageLogs)
-                .Include(b => b.Service)
-                .SingleAsync(m => m.Id == id);
-
-            if (booking == null)
-            {
-                return HttpNotFound();
-            }
-
-            int userId;
-
-            if (!int.TryParse(HttpContext.User.GetUserId(), out userId))
-            {
-                //XXX:TODO Gracefully handle this
-                throw new Exception("Unable to get logged in User Id.");
-            }
-
-            if (ModelState.IsValid)
-            {
-                booking.Package.DeliveredByUserId = model.UserId;
-                booking.Package.Status = PackageStatus.Delivered;
-
-                var packageLog = new PackageLog()
-                {
-                    PackageId = booking.Package.BookingId,
-                    LogMessage = string.Format("Delivered To Consignee by Courier {0}.", model.UserId),
-                    LoggedAt = DateTime.Now
-                };
-
-                booking.Package.PackageLogs.Add(packageLog);
-
-                _context.Update(booking);
-
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction("Package", "Booking", new { id = booking.Id });
-            }
-
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
 
             return View(model);
         }
---
+++ src/SwiftCourier/Controllers/PackagesController.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using SwiftCourier.Models;
+using SwiftCourier.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SwiftCourier.Controllers
+{
+    public class PackagesController : BaseController
+    {
+        private ApplicationDbContext _context;
+
+        public PackagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var bookings = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Package)
+                .Include(b => b.Invoice)
+                .Include(b => b.CreatedBy)
+                .Include(b => b.Service).ToListAsync();
+
+            return View(bookings.ToListViewModel());
+        }
+
+        public async Task<IActionResult> Dispatch(int id)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Service)
+                .SingleAsync(m => m.Id == id);
+
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Dispatch(int id, DispatchViewModel model)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Package.PackageLogs)
+                .Include(b => b.Service)
+                .SingleAsync(m => m.Id == id);
+
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            int userId;
+
+            if (!int.TryParse(HttpContext.User.GetUserId(), out userId))
+            {
+                //XXX:TODO Gracefully handle this
+                throw new Exception("Unable to get logged in User Id.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                booking.Package.AssignedToUserId = model.UserId;
+
+                //XXX:TODO Fix when couriers are able to receive packages to their personal inventory
+                // Update status to OutForDelivery when the courier has confirmed receipt of the package
+                // For now, set as out for delivery when package is dispatched to courier
+                //booking.Package.Status = PackageStatus.DispatchedToCourier;
+                booking.Package.Status = PackageStatus.OutForDelivery;
+
+                var packageLog = new PackageLog()
+                {
+                    PackageId = booking.Package.BookingId,
+                    //XXX:TODO See comment above
+                    //LogMessage = "Dispatched To Courier",
+                    LogMessage = string.Format("Received By Courier {0}.", model.UserId),
+                    LoggedAt = DateTime.Now
+                };
+
+                booking.Package.PackageLogs.Add(packageLog);
+
+                _context.Update(booking);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Details", "Packages", new { id = booking.Id });
+            }
+
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Deliver(int id)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Service)
+                .SingleAsync(m => m.Id == id);
+
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Deliver(int id, DeliverViewModel model)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Package.PackageLogs)
+                .Include(b => b.Service)
+                .SingleAsync(m => m.Id == id);
+
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            int userId;
+
+            if (!int.TryParse(HttpContext.User.GetUserId(), out userId))
+            {
+                //XXX:TODO Gracefully handle this
+                throw new Exception("Unable to get logged in User Id.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                booking.Package.DeliveredByUserId = model.UserId;
+                booking.Package.Status = PackageStatus.Delivered;
+
+                var packageLog = new PackageLog()
+                {
+                    PackageId = booking.Package.BookingId,
+                    LogMessage = string.Format("Delivered To Consignee by Courier {0}.", model.UserId),
+                    LoggedAt = DateTime.Now
+                };
+
+                booking.Package.PackageLogs.Add(packageLog);
+
+                _context.Update(booking);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Details", "Packages", new { id = booking.Id });
+            }
+
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> BillOfLading(int id)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Service)
+                .Include(b => b.CreatedBy)
+                .SingleAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(booking.ToDetailsViewModel());
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var booking = await _context.Bookings
+                .Include(b => b.Customer)
+                .Include(b => b.Invoice)
+                .Include(b => b.Package)
+                .Include(b => b.Package.PackageLogs)
+                .Include(b => b.Service)
+                .Include(b => b.CreatedBy)
+                .SingleAsync(m => m.Id == id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(booking.ToDetailsViewModel());
+        }
+    }
+}
