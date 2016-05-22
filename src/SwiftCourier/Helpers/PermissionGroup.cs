da63cf4cc55cd1272c00b8aaf20f28c517cdf1d0
Robert Campbell <waprave@gmail.com>
2016-05-22 13:51:34 -0500

    Added general to settings group
    Added general to settings group

--- src/SwiftCourier/Helpers/PermissionGroup.cs
+++ src/SwiftCourier/Helpers/PermissionGroup.cs
@@ -17,7 +17,8 @@ namespace SwiftCourier.Helpers
                 new PermissionGroup() { Id = "user_management", Name = "User Management" },
                 new PermissionGroup() { Id = "bookings", Name = "Bookings" },
                 new PermissionGroup() { Id = "customer_management", Name = "Customer Management" },
-                new PermissionGroup() { Id = "settings", Name = "Settings" }
+                new PermissionGroup() { Id = "settings", Name = "Settings" },
+                new PermissionGroup() { Id = "general", Name = "General" }
             };
 
             return groups;
